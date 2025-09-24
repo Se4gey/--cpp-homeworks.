0fd998f17b8982d89f747cd4cf6df39f87f07d18
Se4gey <valov.05@bk.ru>
2025-09-24 19:32:59 +0500

    Add files via upload

---
+++ homework 1.cpp
@@ -0,0 +1,16 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+// Sample C++ code
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "hello world" << endl;
+    return 0;
+}
